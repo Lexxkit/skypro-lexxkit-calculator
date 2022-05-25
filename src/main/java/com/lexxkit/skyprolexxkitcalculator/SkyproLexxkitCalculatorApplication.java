b33ae5c1e0fc711a38a232ec506e91734a5ade9a
Lexxkit <lexxkit@gmail.com>
2022-05-25 14:21:37 +0300

    init

---
+++ src/main/java/com/lexxkit/skyprolexxkitcalculator/SkyproLexxkitCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.lexxkit.skyprolexxkitcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproLexxkitCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproLexxkitCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lexxkit/skyprolexxkitcalculator/SkyproLexxkitCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lexxkit.skyprolexxkitcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproLexxkitCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
